b522067ba763439e1eb7c55cbc4435dc0eb2ffbc
alanyw <an1009400811@126.com>
2017-04-30 21:32:28 +0800

    add static file and index.html

--- Blog/settings.py
+++ Blog/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'article',
+    'Blog'
 ]
 
 MIDDLEWARE = [
@@ -71,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Blog.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -85,7 +83,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -119,8 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFIES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+#     '/var/www/static',
+# )
---
+++ Blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from article.models import Article
+
+
+def index(request):
+    latest_article_list = Article.objects.order_by("pub_date")
+    title = Article.objects.order_by("title")
+    context = {
+        'latest_article_list': latest_article_list,
+        'title': title,
+    }
+    return render(request, 'Blog/index.html', context)
