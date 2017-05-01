7edb660ef4808093184231b9e3ab37560cc791a7
alanyw <an1009400811@126.com>
2017-05-01 23:51:36 +0800

    remove a color and add alanyw.js in static

---
+++ Blog/static/js/alanyw.js
@@ -0,0 +1,9 @@
+/**
+ * Created by alan on 17-5-1.
+ */
+$(document).ready(function () {
+            $(".article_intro:odd").css({"background-color":"#03af6c","color":"#ffffff"});
+            $(".article_intro a:odd").css({"background-color":"#03af6c","color":"#ffffff"});
+            $(".line:odd").css("background-color","#ffffff")
+
+        })
